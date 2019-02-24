1b3b7a82715fab4a34cecc3b7df615a5f0c88c32
陈刚 <769063757@qq.com>
2019-02-24 17:38:52 +0800

    ll

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './style/base.css'
 import './style/index.css'
 // import './style/instroduction.less'
-// require('./js/head.js')
-// require('./js/banner.js')
-// require('./js/foot.js')
+require('./js/head.js')
+require('./js/banner.js')
+require('./js/foot.js')
 require('./js/aboutList.js')
